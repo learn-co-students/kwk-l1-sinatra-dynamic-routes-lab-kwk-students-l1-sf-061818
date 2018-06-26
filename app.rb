3938d30507808a94fb5614535d0589f9ec3157af
jackielim17 <jxckielim17@gmail.com>
2018-06-26 21:31:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,49 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-get '/reversename/:name'
-@name = params[:name].reverse
-"#{@name}"
+get "/reversename/:name" do
+@name = params[:name]
+"#{@name.reverse}"
+
+end
+
+get '/square/:number' do
+@number = params[:number].to_i
+@product = @number * @number
+"#{@product}"
 end 
+
+ get '/say/:number/:phrase' do
+   @number = params[:number].to_i
+   @phrase = params[:phrase].to_s
+   @phrase*@number
+ end 
+ get '/say/:word1/:word2/:word3/:word4/:word5' do
+   @word1 =params[:word1]
+   @word2 = params[:word2]
+   @word3 = params[:word3]
+   @word4 = params[:word4]
+   @word5 = params[:word5]
+   @word1 + " " +@word2+ " " +@word3+ " " +@word4+ " " +@word5+ "."
+ end 
+ get '/:operation/:number1/:number2' do
+   @operation = params[:operation].to_s
+   @number1 = params[:number1].to_i
+   @number2 = params[:number2].to_i
+   if @operation == "add"
+     @add = @number1 + @number2
+     "#{@add}"
+       elsif @operation == "subtract"
+     @subtract = @number1 - @number2
+     "#{@subtract}"
+     elsif @operation == "multiply"
+     @multiply= @number1 * @number2
+     "#{@multiply}"
+     elsif @operation == "divide"
+     @divide = @number1 / @number2
+     "#{@divide}"
+   
+   else 
+   end 
+ end 
 end
